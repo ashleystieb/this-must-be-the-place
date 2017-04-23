a24743f6a1a711a45c9a2b75901149bda7ee67ba
ashleystieb <ashleystieb@gmail.com>
2017-04-22 19:42:10 -0700

    updated javascript

--- eat/eat/settings.py
+++ eat/eat/settings.py
@@ -118,6 +118,7 @@ USE_L10N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
@@ -129,6 +130,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'get_restaurant', "static"),
 
 ]
-
-
-
--- eat/get_restaurant/static/js/get_restaurant.js
+++ eat/get_restaurant/static/js/get_restaurant.js
@@ -2,94 +2,170 @@
  * Created by ashley on 4/12/17.
  */
 
-    /* Keys
-    JS PLACES API KEY = AIzaSyD64gDwd84RCIDl3eNnpmzsvPD8u2u_UpY
-    MAPS API KEY = AIzaSyCpkXptIGyLa_jIaStEFTxw3oO7Dq-nQbU
-    JS MAPS API KEY = AIzaSyDfwZii7p9ZyqnlQmx8vDgq4Oh_D7h9ZvU
-    GEOCODE API KEY = AIzaSyAgvTmt9cQWZpT5im88n6_exM9zVEzopwo
+/* Keys
+ JS PLACES API KEY = AIzaSyD64gDwd84RCIDl3eNnpmzsvPD8u2u_UpY
+ MAPS API KEY = AIzaSyCpkXptIGyLa_jIaStEFTxw3oO7Dq-nQbU
+ JS MAPS API KEY = AIzaSyDfwZii7p9ZyqnlQmx8vDgq4Oh_D7h9ZvU
+ GEOCODE API KEY = AIzaSyAgvTmt9cQWZpT5im88n6_exM9zVEzopwo
 
-    */
+ */
 
-$(document).ready(function() {
-    console.log('hello javascript');
-    if(jQuery){
-        console.log('jQuery');
+$(document).ready(function () {
+
+    // Global variables
+
+    var lat;
+    var long;
+    var restaurantsLow = [];
+    var restaurantsMed = [];
+    var restaurantsHigh = [];
+
+
+    console.log('JavaScript detected');
+    if (jQuery) {
+        console.log('jQuery detected');
     }
 
-/*
-    function geoCodeLocation(latitude, longitude) {
-        $.ajax({
-            type: 'GET',
-            url: 'https://developer.trimet.org/ws/V1/stops?ll=' + longitude + ',' + latitude + '&meters=500&appID=823A27BED3E0A66DABBD63446&json=true',
-            success: function(response) {
-                var myLoc = response.resultSet.location;
-                var lat = [];
-                var lng = [];
-                var info = [];
-                var markers = [];
-                $.each(myLoc, function (i) {
-                    // Add latitude to array
-                    lat.push(myLoc[i].lat);
-                    // Add longitude to array
-                    lng.push(myLoc[i].lng);
-                    info.push(myLoc[i].desc);
-                });
-                // Adds latitude and longitude to array
-                markers.push(lat, lng);
-                // Adds markers to map
-                addMarker(markers, latitude, longitude, info);
-                // Console logs latitude and longitude
-                console.log(markers);
-            },
-            error: function () {
-                alert('Error loading page request');
-            }
-        });
-    }*/
+    // Converts addresses to latitude longitude coordinates
 
-function getLatLong(address){
-    console.log('clicked');
-        // Takes user input to get address
-        // var $cuisine = $('#cuisine').val();
-        // var $price = $('#price').val();
+    function getLatLong(address) {
         var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=AIzaSyAgvTmt9cQWZpT5im88n6_exM9zVEzopwo';
-        $.ajax({
-            async: 'false',
+        return $.ajax({
             type: 'GET',
             url: url,
-            success: function(response){
+            success: function (response) {
                 // Gets latitude and longitude
-                var lat = response.results[0].geometry.location.lat;
-                var long = response.results[0].geometry.location.lng;
+
+                lat = response.results[0].geometry.location.lat;
+                long = response.results[0].geometry.location.lng;
                 // Plugs latitude and longitude into function
-                console.log(lat,long);
-                $('#latitude').html(lat);
-                $('#longitude').html(long);
             }
         });
-}
-function getRestaurant(latitude,longitude,cuisine) {
-    var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + latitude + ',' + longitude + '&radius=5000&type=restaurant&keyword=' + cuisine + '&key=AIzaSyD64gDwd84RCIDl3eNnpmzsvPD8u2u_UpY';
-    $.ajax({
-        type: 'GET',
-        url: url,
-        success: function (response) {
-            var name = response.results[0].name;
-            console.log(name);
+    }
+
+    function getRestaurant(lat, long, keyword) {
+        // var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=5000&type=restaurant&keyword=mexican&key=AIzaSyD64gDwd84RCIDl3eNnpmzsvPD8u2u_UpY';
+        var params = {
+            'location': new google.maps.LatLng(lat, long),
+            'radius': '25000',
+            'type': 'restaurant',
+            'keyword': keyword,
+            'key': 'AIzaSyD64gDwd84RCIDl3eNnpmzsvPD8u2u_UpY',
+            'pagetoken': 'next_page_token'
+        };
+        service = new google.maps.places.PlacesService($('#blank').get(0));
+        service.nearbySearch(params, function (results, status) {
+
+            //console.log(results);
+
+            // Checks price range for each result and adds to corresponding list
+
+            console.log(results);
+
+
+            for (i in results) {
+
+
+                if (results[i].price_level === 1 || results[i].price_level === 2) {
+                    restaurantsLow.push(results[i]);
+
+                } else if (results[i].price_level === 3) {
+                    restaurantsMed.push(results[i]);
+
+                } else if (results[i].price_level === 4) {
+                    restaurantsHigh.push(results[i]);
+
+                } else {
+                    restaurantsMed.push(results[i]);
+                }
+            }
+
+            // Randomly chooses restaurant based on desired price range
+
+            var $price = $('#price').val();
+            var place = getResult($price);
+
+            // Gets photo from randomly chosen restaurant
+
+            var photo = place.photos[0].getUrl({'maxWidth': 500, 'maxHeight': 500});
+            $('#image').attr('src', photo);
+            console.log(photo);
+
+            var placeid = place.place_id;
+            getPlaceDetails(placeid);
+        });
+    }
+
+    // Gets details of randomly selected restaurant
+    function getPlaceDetails(placeid) {
+        var params = {
+            'placeId': placeid,
+            'key': 'AIzaSyD64gDwd84RCIDl3eNnpmzsvPD8u2u_UpY'
+        };
+        service = new google.maps.places.PlacesService($('#blank2').get(0));
+        service.getDetails(params, function (results, status) {
+
+            console.log(results);
+
+            // Adds address to website
+            if (results.hasOwnProperty('adr_address')) {
+                $('#address').html(results.adr_address);
+            } else {
+                $('#address').html('No address listed');
+            }
+
+            // Adds website link
+            if (results.hasOwnProperty('website')) {
+                $("#web").attr("href", results.website);
+                $('#website').css({'display':'block'});
+            } else {
+                $("#web").attr("href", results.url);
+                $('#website').css({'display':'block'});
+            }
+    });
+    }
+
+    function getResult(price) {
+
+        // Checks price entered and length of array
+
+        if (price === '$' && restaurantsLow.length > 0) {
+
+            // Chooses random restaurant and adds it to HTML
+
+            var lowChoice = (restaurantsLow[Math.floor(Math.random() * restaurantsLow.length)]);
+            //var random = (Math.floor(Math.random() * restaurantsLow.length) + 1) - 1;
+            //var lowChoice = restaurantsLow[random];
+            $('#name').html(lowChoice.name);
+            console.log(restaurantsLow);
+            return lowChoice;
+
+        } else if (price === '$$' && restaurantsMed.length > 0) {
+            var medChoice = (restaurantsMed[Math.floor(Math.random() * restaurantsMed.length)]);
+            $('#name').html(medChoice.name);
+            console.log(restaurantsMed);
+            return medChoice;
+
+        } else if (price === '$$$' && restaurantsHigh.length > 0) {
+            var highChoice = (restaurantsHigh[Math.floor(Math.random() * restaurantsHigh.length)]);
+            $('#name').html(highChoice.name);
+            console.log(restaurantsHigh);
+            return highChoice;
+
+        } else {
+            $('#name').html('No results.');
+            return 'None';
         }
-    });
-}
+    }
 
-    $('#submit').on('click', function(){
+
+    $('#submit').on('click', function () {
         var $address = $('#location').val();
-        getLatLong($address);
+        // Changes location to latitude and longitude
+        $.when(getLatLong($address)).done(function () {
+            var $cuisine = $('#cuisine').val();
+            // Compiles a list of restaurants based on criteria
+            getRestaurant(lat, long, $cuisine);
+        });
     });
-
-    $('#restaurant').on('click', function(){
-        //var $cuisine = $('#cuisine').val();
-        getRestaurant('45.5230622','-122.6764816','mexican');
-
-    });
-
 });
-
